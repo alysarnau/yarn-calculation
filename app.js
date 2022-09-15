997a50bcc75552a6f57db26b136c52d0f3a5c1eb
alysvolatile <alice.g.cooper@gmail.com>
2022-09-15 16:30:12 -0400

    Create function for calculate and updateDisplay

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 console.log('connected!')
+
+let originalLength;
+let lengthType;
+let originalWeight;
+let weightType;
+let displaySpan;
+
+function calculateLengthPerWeight(length, weight) {
+    // divide original length by weight
+    // define result
+    let result = length/weight
+    return result;
+    // return result
+}
+
+function updateDisplay(result) {
+    // convert result that to a useful string
+    if (originalLength === 'meter' && originalWeight === 'oz') {
+        displaySpan = `meters/oz`
+    } else if (originalLength === 'yard' && originalWeight === 'oz' ) {
+        displaySpan = `yards/oz`
+    } else if (originalLength === 'meter' && originalWeight === 'gram' ) {
+        displaySpan = `meters/gram`
+    } else {
+        displaySpan = `yards/gram`
+    }
+}
