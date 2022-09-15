04ebb703a68844d0b03702fcb0492b50677ee60d
alysvolatile <alice.g.cooper@gmail.com>
2022-09-15 17:04:21 -0400

    original length and weight save properly

--- app.js
+++ app.js
@@ -6,19 +6,21 @@ let originalWeight;
 let weightType;
 let displaySpan;
 
-function calculateLengthPerWeight(length, weight) {
+
+
+function calculateLengthPerWeight() {
     // divide original length by weight
-    // define result
-    let result = length/weight
-    return result;
+
+    let result = originalLength/originalWeight
     // return result
+    return result;
 }
 
 function updateDisplay(result) {
     // convert result that to a useful string
-    if (originalLength === 'meter' && originalWeight === 'oz') {
+    if (originalLength === 'meter' && originalWeight === 'ounce') {
         displaySpan = `meters/oz`
-    } else if (originalLength === 'yard' && originalWeight === 'oz' ) {
+    } else if (originalLength === 'yard' && originalWeight === 'ounce' ) {
         displaySpan = `yards/oz`
     } else if (originalLength === 'meter' && originalWeight === 'gram' ) {
         displaySpan = `meters/gram`
@@ -26,3 +28,16 @@ function updateDisplay(result) {
         displaySpan = `yards/gram`
     }
 }
+
+function onSubmit(e) {
+    e.preventDefault();
+    // weightType = document.getElementById("weight-type").value;
+    originalWeight = document.getElementById("original-weight").value;
+    // lengthType = document.getElementById("length-type").value;
+    originalLength = document.getElementById("original-length").value;
+    console.log('click')
+    console.log(`original length: ${originalLength}`)
+    console.log(`original weight: ${originalWeight}`)
+}
+
+document.getElementById("calculate-form").addEventListener("submit", onSubmit)
